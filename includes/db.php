82ecf9ca0485f3cfad9c4912639b76b61370cd97
abraham <youngtitan700@gmail.com>
2026-05-30 20:34:29 -0300

    db

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "exit_exam_system";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$user = "user_34adb6fa";
+$pass = "pw_e5adcc49";
+$db   = "db_ec0868aa";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
