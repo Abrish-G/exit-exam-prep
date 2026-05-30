20879ddf7d666e2925669ce641957b6fa6f0a6f2
abraham <youngtitan700@gmail.com>
2026-05-30 20:46:42 -0300

    modified:   includes/db.php

--- includes/db.php
+++ includes/db.php
@@ -4,8 +4,9 @@ $host = "db.fr-pari1.bengt.wasmernet.com";
 $user = "user_34adb6fa";
 $pass = "pw_e5adcc49";
 $db   = "db_ec0868aa";
+$port = 10272;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
